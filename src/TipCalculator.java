d5b0afa3bb9afba122e6e8d244b7aea20990a384
bt_1n3_25 <sample@bths.edu>
2023-09-21 08:42:22 -0400

    Added while loop with total cost in it

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,10 +3,11 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        double totalCost;
+        double totalCost = 0;
         int peopleNum;
         double tipPercent;
         int temp;
+        double input = 0;
 
         System.out.println("Welcome to the Tip Calculator!");
         System.out.print("How many people are in your group? ");
@@ -15,5 +16,15 @@ public class TipCalculator {
         System.out.print("What's the tip percentage? (0-100) ");
         temp = scan.nextInt();
         tipPercent = temp / 100.0;
+
+        while (input != -1) {
+            System.out.print("Enter a cost in dollars and cents, e.g 12.50 (-1 to end) ");
+            input = scan.nextDouble();
+            if (input != -1 ) {
+                totalCost = totalCost + input;
+            }
+        }
+
+        System.out.println("Bill before tip " + totalCost);
     }
 }
